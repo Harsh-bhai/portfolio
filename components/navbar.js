6c871fea973e67b537a9bce5aa95949cb0f03243
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2022-10-06 18:46:39 +0530

    updated
    updated

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,12 @@
+// yarn add react-icons --save
+// yarn add hamburger-react
+
 import React from 'react'
 import { useRef } from 'react';
 import { MdAccountCircle } from 'react-icons/md';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiFillCloseCircle } from 'react-icons/ai';
 import Link from 'next/link';
-import Hamburger from 'hamburger-react'
 const Navbar = () => {
   const toggle = () => {
     if ((ref.current.classList.contains("-translate-x-full"))) {
@@ -14,18 +18,18 @@ const Navbar = () => {
       ref.current.classList.add("-translate-x-full")
     }
   }
-
- 
   const ref = useRef()
   return (
     <div>
-      <div className="navbar flex flex-col md:flex-row items-center bg-gradient-to-r from-cyan-400 via-cyan-200 to-cyan-100  md:p-2">
-     <div  className='z-30 top-0 left-0 absolute m-7 md:m-10 text-3xl font-thin md:hidden'> <Hamburger  onToggle={toggle} /></div>
+      <div className="navbar flex flex-col md:flex-row items-center  md:py-3  py-2 ">
+     <div className=' top-0 left-0 absolute m-7 md:m-10 text-3xl font-thin md:hidden'> <GiHamburgerMenu onClick={toggle}  /></div>
         <Link href={"/"}><div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 md:z-10 my-4 md:my-0 mx-10 cursor-pointer">
-          {/* <img src="/logo.png" alt="" /> */}
-          <span className="ml-3 text-xl mt-6 md:mt-0">Portfolio</span>
+          {/* <img src="/logo.png" className='h-12 ' alt="" /> */}
+          <span className="ml-3 text-xl">Portfolio</span>
         </div></Link>
-        <div ref={ref} className="navitems flex flex-col md:flex-row md:items-center md:bg-inherit bg-cyan-300 px-10 py-4 md:py-0 absolute top-0 left-0 transform transition-transform -translate-x-full h-full md:h-auto md:w-full w-2/3 md:static md:translate-x-0  md:transition-none ease-in-out" >
+        <div ref={ref} className="navitems flex flex-col md:flex-row md:items-center md:bg-inherit bg-cyan-300 px-10 py-4 md:py-0 absolute top-0 left-0 transform transition-transform -translate-x-full h-full md:h-auto md:w-full w-2/3 md:static md:translate-x-0  md:transition-none ease-in-out z-20" >
+          <span className='absolute top-8 left-8
+          'onClick={toggle} ><AiFillCloseCircle className='text-4xl font-extralight md:hidden' /></span>
           <div className="flex flex-col md:flex-row md:items-center md:space-x-8  mainitems my-24 md:my-4 space-y-6 md:space-y-0">
             <Link  href={"/about"}><div onClick={toggle} className='hover:text-cyan-600 text-gray-700 cursor-pointer'>About</div></Link>
             <Link  href={"/skills"}><div onClick={toggle} className='hover:text-cyan-600 text-gray-700 cursor-pointer'>Skills</div></Link>
@@ -33,7 +37,8 @@ const Navbar = () => {
             <Link  href={"/contact"}><div onClick={toggle} className='hover:text-cyan-600 text-gray-700 cursor-pointer'>Contact Us</div></Link>
           </div>
         </div>
-        <div className=" account absolute top-2 md:top-0 right-0 text-4xl p-6 cursor-pointer ">
+        {/* <button class=" text-white bg-cyan-500  mr-40  py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button> */}
+        <div className=" z-20 account absolute top-1  md:top-[-5px] right-0 text-4xl p-6 cursor-pointer ">
           <MdAccountCircle />
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 export default function Home() {
   return (
     <div>
       <section className="text-gray-600 body-font">
   <div className="container mx-auto flex px-5 py-24 md:py-4 md:flex-row flex-col items-center">
-    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center mx-32">
+    <div className="lg:flex-grow md:w-2/3 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center mx-32">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 animate-fadein">Hi! I am Harsh Dewangan
         <p className='text-sm text-cyan-800'>Web Developer, Programmer, Student</p>
       </h1>
       <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
       <div className="flex justify-center">
-        <button className=" text-white bg-cyan-500 border-0 py-2 px-6 focus:outline-none hover:bg-cyan-600 rounded-full text-xs md:text-lg">Hire Me</button>
+        <Link href={"/contact"}><button className=" text-white bg-cyan-500 border-0 py-2 px-6 focus:outline-none hover:bg-cyan-600 rounded-full text-xs md:text-lg">Hire Me</button></Link>
         <button className="ml-4  text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded-full text-xs md:text-lg">Resume</button>
       </div>
     </div>
