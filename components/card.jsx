ebf5f313d702d80accbdbcb524ceab14504d82f4
Harsh Dewangan <harshdewangan2019@gmail.com>
2024-04-23 20:01:56 +0530

    chatapp added

--- components/card.jsx
+++ components/card.jsx
@@ -156,20 +156,6 @@ const Card = () => {
               <p className="leading-relaxed text-base text-cyan-500">
                 {item[1].desc}
               </p>
-              <div className="text-cyan-800 cursor-pointer font-bold hover:text-cyan-600 inline-flex items-center">
-                Learn More
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </div>
             </div>
           </div>
         );
